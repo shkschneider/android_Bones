960534a7663ac5ed099ac901d5cd7eb2bfba7a6b
alan.schneider <alan.schneider@adevinta.com>
2020-04-24 12:50:17 +0200

    First attempt using a Delegate
    Changes to the object wont be saved automatically

--- demo/src/main/kotlin/me/shkschneider/bones/Application.kt
+++ demo/src/main/kotlin/me/shkschneider/bones/Application.kt
@@ -2,9 +2,20 @@ package me.shkschneider.bones
 
 object Application {
 
+    private var bone by BoneDelegate(Bone.serializer())
+    private val clavicle = Bone.clavicle
+
     @JvmStatic
     fun main(vararg argv: String) {
         println("Bones!")
+        println("What I start with: $clavicle")
+        bone = clavicle
+        println("What my bone looks like: $bone")
+        bone = null
+        bone = clavicle
+        println("What my bone still looks like: $bone")
+        bone?.isBroken = true
+        println("What my broken bone looks like: $bone")
     }
 
 }
--- library/src/main/kotlin/me/shkschneider/bones/BoneDelegate.kt
+++ library/src/main/kotlin/me/shkschneider/bones/BoneDelegate.kt
@@ -3,23 +3,41 @@ package me.shkschneider.bones
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.json.JsonElement
 import kotlin.reflect.KProperty
 
 class BoneDelegate<T : Any>(private val serializer: KSerializer<T>) {
 
-    private val provider = Json(JsonConfiguration.Stable)
-    private var json: String? = null
+    companion object {
+
+        private val provider = Json(JsonConfiguration.Stable)
+
+    }
+
+    private var json: JsonElement? = null
 
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T? =
-        json?.let {
-            val jsonElement = provider.parseJson(it)
-            provider.fromJson(serializer, jsonElement)
-        }
+        jsonToObject(json)
 
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T?) {
-        json = value?.let {
-            provider.toJson(serializer, value).toString()
-        }
+        json = objectToJson(value)
     }
+
+//    private fun stringToJson(string: String?): JsonElement? =
+//        string?.let {
+//            provider.parseJson(it)
+//        }
+
+    private fun jsonToObject(jsonElement: JsonElement?): T? =
+        jsonElement?.let {
+            provider.fromJson(serializer, it)
+        }
+
+    private fun objectToJson(obj: T?): JsonElement? =
+        obj?.let {
+            provider.toJson(serializer, it)
+        }
+
+    override fun toString(): String = json.toString()
 
 }
